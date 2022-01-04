f2f0a445cc08a01f1d57f1c2893e4e376cdb1da1
Sumaya32 <71770321+Sumaya32@users.noreply.github.com>
2022-01-04 22:21:38 +0100

    Update3

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import * as styles from "./header.module.css"
+import { StaticImage } from "gatsby-plugin-image";
 
 const Header = () => (
   <header className={styles.headerContainer} >
-      <a className={styles.imgLogo} href="/"> <img className={styles.logo} src="/logo1.png"></img></a>
+      <a className={styles.imgLogo} href="/"> <StaticImage className={styles.logo} src="../images/logo1.png"/></a>
         <Link to="/" className={styles.item} >Home</Link>
         <Link to="/cocktails" className={styles.item}>Cocktails</Link>
         <Link to="/about" className={styles.item}>About</Link>
